5e518aea8271e3b9171619d51bd7a6527fc2a059
Samathy Barratt <samathy@sbarratt.co.uk>
2019-06-14 17:37:08 +0100

    Configuration addition for integration tests

---
+++ resources/source.c
@@ -0,0 +1,5 @@
+if ( foo = bar )
+{
+    int given = 10;
+    bool stGeorge = 0xDEADBEEF;
+}
---
+++ source/test_main.d
@@ -0,0 +1,25 @@
+import c_lex.lexer: lexer, stateException;
+import c_lex.mmrangefile: mmrangefile;
+import std.stdio: writeln;
+
+int main()
+{
+
+    auto source = new mmrangefile("resources/source.c");
+
+    auto l = new lexer!(mmrangefile, char)(source);
+
+    try
+    {
+        l.scan();
+    }
+    catch (stateException e)
+    {
+        writeln(e.msg);
+    }
+
+    writeln(l.get_token_list());
+
+    return 0;
+
+}
