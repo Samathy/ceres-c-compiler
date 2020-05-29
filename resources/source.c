fc430ce97baff6c611a8422902f80d3ee73a37e6
Samathy Barratt <samathy@sbarratt.co.uk>
2020-05-29 16:47:12 +0100

    Update test source file with more nonsense

--- resources/source.c
+++ resources/source.c
@@ -3,6 +3,7 @@ int main()
 
     int foo = 0;
     int bar = 1;
+    unsigned char wibble;
 
     if ( foo = bar )
     {
@@ -10,6 +11,29 @@ int main()
         unsigned int stGeorge = 0xDEADBEEF;
     }
 
+    printf(foo);
+
     return 0;
 
 }
+
+char horse(int donkey)
+{
+    int fourteen = 15;
+
+    if (donkey > 10)
+    {
+        return donkey - 10;
+    }
+
+    return donkey+10-fourteen;
+}
+
+char * nonsense()
+{
+    char * p;
+    p = 0x100;
+
+    return p>>10;
+}
+
