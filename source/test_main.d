955d66161c9cfaa9b9cc63303dc4b061415e7c0f
Samathy Barratt <samathy@sbarratt.co.uk>
2020-11-27 19:15:44 +0000

    Add getopt to cerescc so we can test with more inputs

--- source/test_main.d
+++ source/test_main.d
@@ -1,11 +1,25 @@
 import ceres.lexer.lexer : lexer, stateException;
 import ceres.lexer.mmrangefile : mmrangefile;
+import ceres.parser.parser: parser;
 import std.stdio : writeln, File;
 
-int main()
-{
+import std.getopt;
 
-    auto source = new mmrangefile("resources/source.c");
+
+int main(string[] argv)
+{
+    string[] input;
+    bool verbose;
+
+    string arraySep = ",";
+
+    auto helpInformation = getopt(
+            argv,
+            "input", "Input files", &input,
+            "verbose", "Enable verbose output", &verbose,);
+
+
+    auto source = new mmrangefile(input[0]); //Only support one input right now.
 
     auto l = new lexer!(mmrangefile, char)(source);
 
@@ -23,6 +37,11 @@ int main()
     auto f = File("lexer_state_graph.dot", "w");
 
     f.writeln(l.get_state_graph_dot());
+
+    auto p = new parser(l.get_token_list());
+    
+    p.parse();
+
 
     return 0;
 
